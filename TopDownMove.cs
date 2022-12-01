5b3cb3ff82515d808ba9341ed3808091390635c6
Rami <94126625+1Rami1@users.noreply.github.com>
2022-12-01 23:15:32 +0300

    Create TopDownMove.cs

---
+++ TopDownMove.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Move")]
+    [SerializeField] private Rigidbody2D  rb;
+    [SerializeField] private float speed;
+    private Vector2 move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        move.x = Input.GetAxisRaw("Horizontal");
+        move.y = Input.GetAxisRaw("Vertical");
+        move.Normalize();
+
+        rb.velocity = move * speed;
+    }
+}
