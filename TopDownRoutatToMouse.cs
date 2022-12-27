4c0681d8f0a06c06df841e957c40db82d43e98de
Rami <94126625+1Rami1@users.noreply.github.com>
2022-12-27 16:00:51 +0300

    Create TopDownRoutatToMouse.cs

---
+++ TopDownRoutatToMouse.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Move")]
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float speed;
+    private Vector2 move;
+    [Header("mouse")]
+    private Vector2 mouse;
+    
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
+        rb.velocity = move * speed;
+
+        Vector2 look = mouse - rb.position;
+        float angle = Mathf.Atan2(look.y, look.x) * Mathf.Rad2Deg - 90f;
+    }
+    private void Update()
+    {
+        mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
