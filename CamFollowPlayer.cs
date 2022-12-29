2118eed30543844d0b0fa1981099103bb013c21e
Rami <94126625+1Rami1@users.noreply.github.com>
2022-12-29 13:20:19 +0300

    Create CamFollowPlayer.cs

---
+++ CamFollowPlayer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[HelpURL("https://www.youtube.com/watch?v=Sao0j1XNcvM")]
+public class CamFollowPlayer : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float damping;
+    private Vector3 velocity;
+    // Start is called before the first frame update
+    private void FixedUpdate()
+    {
+        Vector3 movePos = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, movePos, ref velocity, damping);
+    }
+}
